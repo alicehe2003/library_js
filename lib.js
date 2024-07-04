87eb1bc3b15bad803b738485b96a283ca64edc7f
alicehe2003 <140996846+alicehe2003@users.noreply.github.com>
2024-07-03 17:30:31 -0700

    Implemented logic to add and display new book from form.

--- lib.js
+++ lib.js
@@ -27,16 +27,14 @@ function closePopUp() {
     document.getElementById("popUp").style.display = "none"; 
 }
 
-function displayBooks() {
-    for (book in myLibrary) {
-        display(book); 
-    }
+function displayBook(book) {
+    let library = document.getElementById("books"); 
 
-    function display(book) {
-        // display singular book 
-        // TODO
-    }
-}
+    let bookDiv = document.createElement('div'); 
+    bookDiv.className = 'book'; 
+    bookDiv.innerHTML = book.info(); 
+    library.appendChild(bookDiv); 
+} 
 
 // get info from form 
 let newBookInfo = document.getElementById("new_book_info"); 
@@ -44,11 +42,15 @@ newBookInfo.addEventListener("submit", (e) => {
     e.preventDefault(); 
 
     // handle submit: add book to library 
-    let title = document.getElementById("book_title"); 
-    let author = document.getElementById("book_author"); 
-    let pages = document.getElementById("book_pages"); 
+    let title = document.getElementById("book_title").value; 
+    let author = document.getElementById("book_author").value; 
+    let pages = document.getElementById("book_pages").value; 
 
-    
+    const newBook = new Book(title, author, pages); 
+    myLibrary.push(newBook); 
+
+    // display updated library (including new book)
+    displayBook(newBook); 
 
     // close pop up 
     closePopUp(); 
